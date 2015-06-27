47a1104a4bf802754425095c76b04bd9f44dad8f
Blake Smith <blakesmithphoto@gmail.com>
2015-06-27 13:02:06 -0700

    Pre Container Fix
    Pre Container fix - working app - style need fixing

---
+++ weatherScript.js
@@ -0,0 +1,35 @@
+
+var postCode = '';
+
+function checkFunction() {
+		var button = document.getElementById('checkbtn');
+		var userInput = document.getElementById('userZip').value;
+
+		postCode = userInput;
+	
+
+	$.ajax({
+		
+		url : "http://api.wunderground.com/api/ee2c13f577bdf0a1/geolookup/conditions/q/US/" + postCode +".json",
+		dataType : "jsonp",
+		success : function(parsed_json) {
+			var location = parsed_json['location']['city'];
+			var temp_f = parsed_json['current_observation']['temp_f'];
+			var wind = parsed_json['current_observation']['wind_mph'];
+			var weather = parsed_json['current_observation']['weather'];
+			var uv = parsed_json['current_observation']['UV'];
+
+			var weatherPlace = document.getElementById('featureOne');
+			weatherPlace.innerHTML = weather;
+
+			var place = document.getElementById('featureTwo');
+			place.innerHTML = temp_f + 'F';
+
+			var windPlace = document.getElementById('featureThree');
+			windPlace.innerHTML = 'Wind ' + wind + ' mpg';
+
+			var uvPlace = document.getElementById('featureFour');
+			uvPlace.innerHTML = 'UV Index ' + uv;
+		}
+	});
+}
