686b122e98e3185f6890f439288c1c6cacb600b5
Blake Smith <blakesmithphoto@gmail.com>
2015-08-22 19:42:47 -0700

    Finished APP with 3 day forecast. Needs responsive / needs forecast weather highs.

--- weatherScript.js
+++ weatherScript.js
@@ -1,5 +1,48 @@
 
+
+
+
 var postCode = '';
+
+// IMAGES
+var $homeImg = $('#bkg_img');
+
+var $clearImg = $('#clear_img');
+var $cloudyImg = $('#cloudy_img');
+var $part_cloudyImg = $('#part_cloudy_img');
+var $rainImg = $('#rain_img');
+
+
+
+$clearImg.hide();
+$cloudyImg.hide();
+$part_cloudyImg.hide();
+$rainImg.hide();
+
+/*
+
+function enterClick() {
+
+	$('#userZip').keypress(function(e) {
+			if (e.which === 13) {
+				runParse();
+				console.log('enter works');
+				return false;
+			}
+	});
+}
+
+*/
+
+
+var holderImg = [];
+
+holderImg.push($homeImg);
+
+
+
+
+// LOGIC _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
 function clearValue() {
 	var elem = document.getElementById('userZip');
@@ -11,11 +54,16 @@ function checkFunction() {
 		var userInput = document.getElementById('userZip').value;
 
 		postCode = userInput;
-	
 
+		runParse();
+}
+
+
+
+function runParse() {
 	$.ajax({
 		
-		url : "http://api.wunderground.com/api/ee2c13f577bdf0a1/geolookup/conditions/q/US/" + postCode +".json",
+		url : "http://api.wunderground.com/api/ee2c13f577bdf0a1/geolookup/conditions/forecast/q/US/" + postCode +".json",
 		dataType : "jsonp",
 		success : function(parsed_json) {
 			var location = parsed_json['location']['city'];
@@ -24,17 +72,79 @@ function checkFunction() {
 			var weather = parsed_json['current_observation']['weather'];
 			var uv = parsed_json['current_observation']['UV'];
 
-			var weatherPlace = document.getElementById('featureOne');
+			var forecast = parsed_json.forecast.txt_forecast.forecastday;
+
+
+				for (index in forecast) {
+    				console.log('Weather forecast for ' + forecast[index]['title'] +
+        				' is ' + forecast[index]['fcttext_metric']);
+				}
+
+				console.log(parsed_json);
+
+			var cityPlace = document.getElementById('featureOne');
+			cityPlace.innerHTML = location;
+
+			var weatherPlace = document.getElementById('featureTwo');
 			weatherPlace.innerHTML = weather;
 
-			var place = document.getElementById('featureTwo');
+			var place = document.getElementById('featureThree');
 			place.innerHTML = temp_f + 'F';
 
-			var windPlace = document.getElementById('featureThree');
+			var windPlace = document.getElementById('featureFour');
 			windPlace.innerHTML = 'Wind ' + wind + ' mpg';
 
-			var uvPlace = document.getElementById('featureFour');
-			uvPlace.innerHTML = 'UV Index ' + uv;
+			//var uvPlace = document.getElementById('featureFour');
+			//uvPlace.innerHTML = 'UV Index ' + uv;
+
+			var day_one = document.getElementById('day1');
+			day_one.innerHTML = forecast[2]['title'] + "<br />" + forecast[2]['icon'];
+
+			var day_two = document.getElementById('day2');
+			day_two.innerHTML = forecast[4]['title'] + "<br />" + forecast[4]['icon'];
+
+			var day_three = document.getElementById('day3');
+			day_three.innerHTML = forecast[6]['title'] + "<br />" + forecast[6]['icon'];
+
+			// WEATHER IMAGE CONDITIONALS _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+			if (weather === 'Clear') {
+				holderImg[0].hide();
+				$clearImg.show();
+
+				holderImg.shift();
+				holderImg.push($clearImg);
+				
+				}
+
+			if (weather === 'Rain' || weather === 'Light Rain') {
+				holderImg[0].hide();
+				$rainImg.show();
+
+				holderImg.shift();
+				holderImg.push($rainImg);
+
+			}
+
+			if (weather === 'Cloudy' || weather === 'Overcast') {
+				holderImg[0].hide();
+				$cloudyImg.show();
+
+				holderImg.shift();
+				holderImg.push($cloudyImg);
+			}
+
+			if (weather === 'Partly Cloudy') {
+				holderImg[0].hide();
+				$cloudyImg.show();
+
+				holderImg.shift();
+				holderImg.push($cloudyImg);
+			}
+
+
 		}
+
 	});
 }
+
